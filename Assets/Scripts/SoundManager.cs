755ccf0089950e41fd059db46becbc0a566ba253
Joe Marchuk <j.marchuk3@gmail.com>
2017-10-08 11:23:22 -0400

    Sound Manager
    Added SoundManager.cs script; quick access to UI SFX and cat SFX;
    includes functions for randomizing cat sounds and avoiding repetition

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+	//UI sounds
+	public AudioClip[] positiveUI;
+	public AudioClip[] neutralUI;
+	public AudioClip[] negativeUI;
+
+	//cat sounds
+	public AudioClip[] happyCat;
+	public AudioClip[] hungryCat;
+	public AudioClip[] angryCat;
+
+	AudioSource source;
+
+	int previousRand = -1;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+
+	void Update () {
+		//for testing cat sounds
+		if (Input.GetKeyDown ("q")) { //press q for random happy cat sound
+			happyCatSound ();
+		}
+
+		if (Input.GetKeyDown ("w")) { //press w for random hungry cat sound
+			hungryCatSound ();
+		}
+
+		if (Input.GetKeyDown ("e")) { //press e for random angry cat sound
+			angryCatSound ();
+		}
+	}
+
+	void happyCatSound(){
+		int randInt = Random.Range (0, happyCat.Length);
+		if (randInt == previousRand) { //ensures same sound never plays twice in a row
+			while (randInt == previousRand) {
+				randInt = Random.Range (0, happyCat.Length);
+			}
+		}
+		source.clip = happyCat [randInt];
+		source.Play ();
+		previousRand = randInt;
+	}
+
+	void hungryCatSound(){
+		int randInt = Random.Range (0, hungryCat.Length);
+		if (randInt == previousRand) { 
+			while (randInt == previousRand) {
+				randInt = Random.Range (0, hungryCat.Length);
+			}
+		}
+		source.clip = hungryCat [randInt];
+		source.Play ();
+		previousRand = randInt;
+	}
+
+	void angryCatSound(){
+		int randInt = Random.Range (0, angryCat.Length);
+		if (randInt == previousRand) { 
+			while (randInt == previousRand) {
+				randInt = Random.Range (0, angryCat.Length);
+			}
+		}
+		source.clip = angryCat [randInt];
+		source.Play ();
+		previousRand = randInt;
+	}
+
+}
